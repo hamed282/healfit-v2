108d0d4f09997bbecd8a8f61fd7caa01a3f2c893
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-14 11:07:46 +0330

    accounts

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, first_name, last_name, email, phone_number=None, trn_number=None, company_name=None, password):
+    def create_user(self, first_name, last_name, email, password, phone_number=None, trn_number=None, company_name=None):
         if not email:
             raise ValueError('Email must be')
 
