aca409766817ae912ab0a887fb56fcad77f44107
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-14 11:42:48 +0330

    accounts

--- A/settings.py
+++ A/settings.py
@@ -116,11 +116,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = "staticfiles/"
+# STATIC_URL = 'static/'
+# STATIC_ROOT = "staticfiles"
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static/'),
 # )
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Default primary key field type
