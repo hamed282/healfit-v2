8cb34a5526c861f1a826c2575a767174163c3ae2
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-17 02:25:48 +0330

    SEO category Blog

--- blog/views.py
+++ blog/views.py
@@ -41,15 +41,28 @@ class RelatedPostView(APIView):
     def get(self, request):
         limit = self.request.query_params.get('limit', None)
         category = self.request.query_params.get('category', None)
+        page = self.request.query_params.get('page', None)
+
+        per_page = 10
+        blog_count = len(BlogModel.objects.all())
+        number_of_pages = math.ceil(blog_count / per_page)
+
         category = get_object_or_404(BlogCategoryModel, category=category)
         if limit is not None:
             blog = BlogModel.objects.filter(category=category)[:int(limit)]
         else:
             blog = BlogModel.objects.filter(category=category)
+
+        if page is not None:
+            page = int(page)
+            blog = blog[per_page*(page-1):per_page*page]
+        else:
+            blog = blog
+
         ser_data = RelatedBlogSerializer(instance=blog, many=True)
 
         ser_meta = MetaCategorySerializer(instance=category)
-        return Response(data={'data': ser_data.data, 'meta': ser_meta.data})
+        return Response(data={'data': ser_data.data, 'meta': ser_meta.data, 'number_of_pages': number_of_pages})
 
 
 
