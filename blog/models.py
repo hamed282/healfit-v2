456765ad91e7c9ca7bd700b8f4f447855baf114a
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-17 01:58:26 +0330

    SEO category Blog

--- blog/models.py
+++ blog/models.py
@@ -41,7 +41,8 @@ class BlogModel(models.Model):
     author = models.CharField(max_length=64)
     role = models.CharField(max_length=24)
     slug = models.SlugField(unique=True)
-    category = models.ForeignKey(BlogCategoryModel, on_delete=models.CASCADE)
+    # category = models.ForeignKey(BlogCategoryModel, on_delete=models.CASCADE)
+    category = models.CharField(max_length=24, blank=True, null=True)
 
     # SEO Fields
     follow = models.BooleanField(default=False)
