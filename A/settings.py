99072898e75d54e4770081020cee7297a8fd74a9
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-17 15:43:12 +0330

    fixed: role accounts

--- A/settings.py
+++ A/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
