0fe69a2337b5909484cfbc7c51633380d7392c00
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-18 10:05:05 +0330

    current address create

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
 
     path('address/', views.UserAddressView.as_view(), name='user_address'),
     path('address/<int:address_id>/', views.UserAddressView.as_view(), name='user_address'),
-    path('currentaddress/<int:user_id>/', views.CurrentAddressView.as_view(), name='user_address'),
+    path('currentaddress/<int:user_id>/', views.CurrentAddressView.as_view(), name='cureent_address'),
+    path('currentaddress/', views.CurrentAddressView.as_view(), name='current_address_create'),
 
     path('info/', views.UserInfoView.as_view(), name='user_info'),
     path('info/<int:user_id>/', views.UserInfoView.as_view(), name='user_info'),
