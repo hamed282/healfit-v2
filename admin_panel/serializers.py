9527cde9f7115921838499bae4b6cd82f647ceda
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-20 14:15:08 +0330

    fixed: tag id blog

--- admin_panel/serializers.py
+++ admin_panel/serializers.py
@@ -145,5 +145,6 @@ class CombinedBlogSerializer(serializers.Serializer):
     def to_representation(self, instance):
         blog_data = BlogModelSerializer(instance).data
         category_data = BlogCategorySerializer(instance.category).data
-        tag_data = AddBlogTagSerializer(instance.blog_tag).data if hasattr(instance, 'blog_tag') else None
-        return {**blog_data, 'category': category_data['category'], 'tag': tag_data['tag'] if tag_data else None}
+        # tag_data = AddBlogTagSerializer(instance.blog_tag).data if hasattr(instance, 'blog_tag') else None
+        # return {**blog_data, 'category': category_data['category'], 'tag': tag_data['tag'] if tag_data else None}
+        return {**blog_data, 'category': category_data['category']}
