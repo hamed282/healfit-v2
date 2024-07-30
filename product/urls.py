df8e0a6e28a1656a874181feb184cf5818f6324f
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-07-30 14:39:27 +0330

    shop product

--- product/urls.py
+++ product/urls.py
@@ -20,5 +20,9 @@ urlpatterns = [
 
     # Shop
     path('items/<slug:slug_product>/', views.ProductItemView.as_view(), name='product_item'),
+    path('variant/', views.ProductVariantShopView.as_view(), name='product_variant'),
+    path('colorimage/', views.ProductColorImageView.as_view(), name='color_image'),
+    path('sizeofcolor/', views.SizeOfColorView.as_view(), name='size_of_color'),
+    path('search_product/', views.SearchProductView.as_view({'get': 'list'}), name='search_product'),
 
 ]
