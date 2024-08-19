ff123ed7c3ee7b34276ca8ac732ada7ca35d1703
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-08-19 11:07:49 +0330

    new database

--- A/settings.py
+++ A/settings.py
@@ -19,12 +19,12 @@ DEBUG = False #os.getenv('DEBUG')
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
     # CORS_ORIGIN_ALLOW_ALL = True
 
