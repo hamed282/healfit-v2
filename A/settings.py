750cfdd555f15c705fb555e7113d8559b321d389
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-08-20 10:17:18 +0330

    new database

--- A/settings.py
+++ A/settings.py
@@ -14,30 +14,30 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # Debug Mode
-DEBUG = True  #os.getenv('DEBUG')
+DEBUG = False  #os.getenv('DEBUG')
 
 if DEBUG:
     ALLOWED_HOSTS = ['*']
 
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('NAME'),
-            'USER': os.getenv('USER'),
-            'PASSWORD': os.getenv('PASSWORD'),
-            'HOST': os.getenv('HOST'),
-            'PORT': os.getenv('PORT'),
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-
-    CORS_ORIGIN_ALLOW_ALL = True
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.mysql',
+    #         'NAME': os.getenv('NAME'),
+    #         'USER': os.getenv('USER'),
+    #         'PASSWORD': os.getenv('PASSWORD'),
+    #         'HOST': os.getenv('HOST'),
+    #         'PORT': os.getenv('PORT'),
+    #     }
+    # }
+    #
+    #
+    # CORS_ORIGIN_ALLOW_ALL = True
 
 else:
     # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
