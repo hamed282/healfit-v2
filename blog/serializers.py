9fa6dae34d8fd12950facd0c42cd3948f3ecd737
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-08-21 17:25:41 +0330

    new database

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,10 +1,14 @@
-from rest_framework.serializers import ModelSerializer, DateField, SerializerMethodField, SlugRelatedField
+from rest_framework import serializers
 from .models import BlogModel, AddBlogTagModel, BlogCategoryModel, BlogImageModel
 
 
-class BlogSerializer(ModelSerializer):
-    tag = SerializerMethodField()
-    category = SlugRelatedField(slug_field='category', queryset=BlogCategoryModel.objects.all(),)
+class BlogSerializer(serializers.ModelSerializer):
+    tag = serializers.SerializerMethodField(required=False, allow_blank=True)
+    category = serializers.SlugRelatedField(slug_field='category', queryset=BlogCategoryModel.objects.all(),)
+    canonical = serializers.CharField(max_length=256, required=False, allow_blank=True)
+    meta_title = serializers.CharField(max_length=60, required=False, allow_null=True)
+    meta_description = serializers.CharField(max_length=150, required=False, allow_null=True)
+    schema_markup = serializers.CharField(max_length=150, required=False, allow_null=True)
 
     class Meta:
         model = BlogModel
@@ -20,29 +24,29 @@ class BlogSerializer(ModelSerializer):
         return tag
 
 
-class BlogAllSerializer(ModelSerializer):
-    created = DateField(format='%d %b %Y')
+class BlogAllSerializer(serializers.ModelSerializer):
+    created = serializers.DateField(format='%d %b %Y')
 
     class Meta:
         model = BlogModel
         fields = ['id', 'cover_image', 'title', 'short_description', 'slug', 'created']
 
 
-class RelatedBlogSerializer(ModelSerializer):
-    created = DateField(format='%d %b %Y')
+class RelatedBlogSerializer(serializers.ModelSerializer):
+    created = serializers.DateField(format='%d %b %Y')
 
     class Meta:
         model = BlogModel
         fields = ['id', 'cover_image', 'title', 'short_description', 'slug', 'created']
 
 
-class MetaCategorySerializer(ModelSerializer):
+class MetaCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogCategoryModel
         fields = '__all__'
 
 
-class ImageBlogSerializer(ModelSerializer):
+class ImageBlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogImageModel
         fields = '__all__'
