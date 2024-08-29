cb8824846f9ca8fe7121d7139a2a17a845909fdb
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-08-29 15:08:01 +0330

    fixed: imagecolor

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1201,34 +1201,6 @@ class ColorImageView(APIView):
 
         return Response(data={'message': 'Create'}, status=status.HTTP_201_CREATED)
 
-    # def put(self, request, product_id):
-    #     product = ProductModel.objects.get(id=product_id)
-    #     sizes = request.data['sizes']
-    #     colors = request.data['colors']
-    #
-    #     for color in colors:
-    #         for size in sizes:
-    #             if not ProductVariantModel.objects.filter(product=product,
-    #                                                       color=ColorProductModel.objects.get(color=color),
-    #                                                       size=SizeProductModel.objects.get(size=size),
-    #                                                       ).exists():
-    #                 ProductVariantModel.objects.create(product=product,
-    #                                                    color=ColorProductModel.objects.get(color=color),
-    #                                                    size=SizeProductModel.objects.get(size=size),
-    #                                                    price=0,
-    #                                                    percent_discount=product.percent_discount,
-    #                                                    quantity=0,
-    #                                                    name=f'{product}-{color}-{size}')
-    #
-    #     variants = ProductVariantModel.objects.filter(product_id=product_id).exclude(
-    #                                                   color__color__in=colors,
-    #                                                   size__size__in=sizes
-    #                                                   )
-    #     print('variant', variants)
-    #     variants.delete()
-    #
-    #     return Response(data={'message': 'Done'}, status=status.HTTP_200_OK)
-
 
 class OrderFilterView(APIView):
     permission_classes = [IsAuthenticated]
