b5c79574e8406b45ef10490bda70183ae7e355ab
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-09-07 09:28:44 +0330

    fixed: TransRef

--- A/settings.py
+++ A/settings.py
@@ -252,7 +252,7 @@ AWS_S3_SIGNATURE_VERSION = os.getenv('AWS_S3_SIGNATURE_VERSION')
 # TELR Settings
 TELR_API_REQUEST = f"https://secure.telr.com/gateway/order.json"
 TELR_API_VERIFY = f"https://secure.telr.com/gateway/order.json"
-TEST = "۱"
+TEST = "1"
 FRAMED = 0
 SOTRE_ID = 29934
 AUTHKEY = 'BnCdX#DGW2P@HHxk'
