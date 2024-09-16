2cd0b720e0c8d7498b6595a53b0de1eb89a32e31
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-09-17 01:40:50 +0330

    feixed: get cookies

--- A/settings.py
+++ A/settings.py
@@ -48,7 +48,7 @@ if DEBUG:
     }
     CORS_ORIGIN_ALLOW_ALL = True
     CORS_ALLOWED_ORIGINS = [
-        'https://healfit.ae',  # دامنه فرانت‌اند شما
+        'https://healfit.ae/',  # دامنه فرانت‌اند شما
     ]
     CORS_ALLOW_CREDENTIALS = True
 
