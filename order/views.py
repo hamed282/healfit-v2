ee13cbabed9cdc1eece37c93f50a5d4970a05a17
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-09-23 09:27:01 +0330

    fixed: add discount threshold in coupon

--- order/views.py
+++ order/views.py
@@ -151,7 +151,8 @@ class OrderPayView(APIView):
                     # email_from = settings.EMAIL_HOST_USER
                     #
                     # send_mail(subject, message_provider, email_from, ['hamed@healfit.ae'])
-                    if not code.infinite:
+
+                    if code and not code.infinite:
                         code.limit -= 1
                         code.save()
                     return Response({'redirect to : ': url}, status=200)
