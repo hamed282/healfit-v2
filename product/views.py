6bc79966c67d84416fde230ecd8dcc9146bb473e
hamed alizadegan <hamed.alizadegan@gmail.com>
2024-09-23 16:54:38 +0330

    fixed: total bag price in add item

--- product/views.py
+++ product/views.py
@@ -436,7 +436,9 @@ class CartView(APIView):
             print(cart)
             print(type(cart))
             for item in cart:
-                print(item["product"]["off_price"])
+                price = item["product"]["off_price"]
+                quantity = item["quantity"]
+                print(quantity)
             product_variant = ProductVariantModel.objects.get(id=product["product"]["id"])
             item_price = Decimal(product_variant.get_off_price()) * product["quantity"]
             # cart_total_price = sum(Decimal(item["product"]["off_price"]) * item["quantity"] for item in cart)
