cb8eaf9248292453b1a358e0af91636acdae97b1
hamed <hamed.alizadegan@gmail.com>
2024-10-19 08:40:42 +0330

    fixed: delivery time

--- order/views.py
+++ order/views.py
@@ -388,15 +388,12 @@ class ShippingView(APIView):
                 delivery_time = datetime.now() + timedelta(days=delivery_day + 1)
             else:
                 delivery_time = datetime.now() + timedelta(days=delivery_day)
-            print('day:', delivery_day)
-            print('zone:', zone)
-            print('1:', delivery_time)
+
             # while delivery_time in uae_holidays or delivery_time.weekday() in [5, 6] and zone != 'Dubai':
             #     delivery_time += timedelta(days=1)
             #     print('2:', delivery_time)
-
-            delivery_time += timedelta(days=holidays_count(order_time, delivery_time))
-            print('3:', delivery_time)
+            if zone != 'Dubai':
+                delivery_time += timedelta(days=holidays_count(order_time, delivery_time))
 
             return delivery_time.strftime('%Y-%m-%d')
 
