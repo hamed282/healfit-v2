7956ad87d653a26f2838c01f9aaf762b74d36c1f
hamed <hamed.alizadegan@gmail.com>
2025-02-23 05:47:44 +0330

    create class and side model

--- product/tasks.py
+++ product/tasks.py
@@ -94,10 +94,10 @@ def zoho_product_update():
                 # product_all = ProductVariantModel.objects.all()
                 # logger.info(product_all)
                 # logger.info(product_all.count())
-                logger.info(name)
-                logger.info(item_id)
-                if i >= 20:
-                    break
+                # logger.info(name)
+                # logger.info(item_id)
+                # if i >= 20:
+                #     break
 
                 if product_variant.exists():
                     # logger.info(name)
@@ -120,7 +120,7 @@ def zoho_product_update():
                                                        quantity=quantity)
                 i += 1
             except Exception as e:
-                # logger.exception("An error occurred")
+                logger.exception("An error occurred")
 
                 product = item['group_name']
                 continue
