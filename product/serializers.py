45bc870753a1f50353186acd62e935d495c5ea24
hamed <hamed.alizadegan@gmail.com>
2025-03-02 08:54:07 +0330

    compression class and side add to variant filter

--- product/serializers.py
+++ product/serializers.py
@@ -104,13 +104,12 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_compression_class(self, obj):
         product = ProductVariantModel.objects.filter(product=obj)  # .order_by('-priority')
 
-        # valid_products = [p for p in product if re.search(r"/CCL.*$", p.name, re.IGNORECASE)]
-        # if not valid_products:
-        #     return []
-        print(product)
+        valid_products
         ccl = set([f'{str(p.compression_class)} - {str(p.compression_class.priority)}' for p in product if p.quantity > 0])
         ccls = sorted(ccl, key=lambda x: int(x.split(" - ")[1]))
-        ccl = [ccl.split(" - ")[0] for ccl in ccls]
+        ccl = [ccl.split(" - ")[ = [p for p in product if re.search(r"/CCL.*$", p.name, re.IGNORECASE)]
+        if not valid_products:
+            return []0] for ccl in ccls]
         return ccl
 
     def get_side(self, obj):
