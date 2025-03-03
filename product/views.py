e0a3219ff896c3cdaf47120039ca30d6691ea002
hamed <hamed.alizadegan@gmail.com>
2025-03-03 19:31:21 +0330

    fixed: product items and variant with add class and side

--- product/views.py
+++ product/views.py
@@ -330,7 +330,7 @@ class ProductNewItemView(APIView):
         product = ProductModel.objects.get(slug=slug_product)
         ser_data = NewProductSerializer(instance=product, context={'compression_class': compression_class,
                                                                         'side': side})
-            return Response(data=ser_data.data, status=status.HTTP_200_OK)
+        return Response(data=ser_data.data, status=status.HTTP_200_OK)
         # except:
         #     return Response(data={'message': 'Page Not Found'}, status=status.HTTP_404_NOT_FOUND)
 
