a46991a20d711c891bc24c028b0809d71a33eb8c
hamed <hamed.alizadegan@gmail.com>
2025-04-03 11:59:41 +0330

    fixed: security

--- A/settings.py
+++ A/settings.py
@@ -277,8 +277,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.healfit.ae'
 EMAIL_HOST_USER = 'no-reply@healfit.ae'
 # DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = 'Ljho,cP4tD#@1'
-EMAIL_PORT = 465
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
 
