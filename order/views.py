9eec088f7ec577557841374aacee471466c0a82f
hamed <hamed.alizadegan@gmail.com>
2025-05-14 12:40:00 +0330

    fixed: shipping

--- order/views.py
+++ order/views.py
@@ -416,7 +416,13 @@ class ShippingView(APIView):
                 return Response(data={'shipping_fee': '0',
                                       'delivery_time': delivery_date(int(shipping.delivery_day), city),
                                       'total_amount': int(amount),
+                                      'subtotal': amount - round(int(amount) / 1.05, 2),
+                                      'vat': round(int(amount) / 1.05, 2),
+
                                       'total_amount_without_discount': int(amount_total) + 0,
+                                      'subtotal_without_discount': amount_total - round(int(amount_total) / 1.05, 2),
+                                      'vat_without_discount': round(int(amount_total) / 1.05, 2),
+
                                       'total_with_shipping': int(amount) + 0
                                       })
             else:
@@ -425,20 +431,38 @@ class ShippingView(APIView):
                     return Response(data={'shipping_fee': shipping.shipping_fee,
                                           'delivery_time': delivery_date(int(shipping.delivery_day), city),
                                           'total_amount': int(amount),
+                                          'subtotal': amount - round(int(amount) / 1.05, 2),
+                                          'vat': round(int(amount) / 1.05, 2),
+
                                           'total_amount_without_discount': int(amount_total) + int(shipping.shipping_fee),
+                                          'subtotal_without_discount': amount_total - round(int(amount_total) / 1.05, 2),
+                                          'vat_without_discount': round(int(amount_total) / 1.05, 2),
+
                                           'total_with_shipping': int(amount) + int(shipping.shipping_fee)
                                           })
                 return Response(data={'shipping_fee': '0',
                                       'delivery_time': delivery_date(int(shipping.delivery_day), city),
                                       'total_amount': int(amount),
+                                      'subtotal': amount - round(int(amount) / 1.05, 2),
+                                      'vat': round(int(amount) / 1.05, 2),
+
                                       'total_amount_without_discount': int(amount_total) + 0,
+                                      'subtotal_without_discount': amount_total - round(int(amount_total) / 1.05, 2),
+                                      'vat_without_discount': round(int(amount_total) / 1.05, 2),
+
                                       'total_with_shipping': int(amount) + 0
                                       })
         else:
             return Response(data={'shipping_fee': '300',
                                   'delivery_day': delivery_date(7),
                                   'total_amount': int(amount),
+                                  'subtotal': amount - round(int(amount) / 1.05, 2),
+                                  'vat': round(int(amount) / 1.05, 2),
+
                                   'total_amount_without_discount': int(amount_total) + 300,
+                                  'subtotal_without_discount': amount_total - round(int(amount_total) / 1.05, 2),
+                                  'vat_without_discount': round(int(amount_total) / 1.05, 2),
+
                                   'total_with_shipping': int(amount) + 300
                                   })
 
