6c4958283d7d46a6e0d61b961931474d5b392806
hamed <hamed.alizadegan@gmail.com>
2025-06-06 09:10:53 +0330

    fixed: unisex filter

--- order/tabby_config.py
+++ order/tabby_config.py
@@ -7,7 +7,10 @@ TABBY_CONFIG = {
     'CURRENCY': 'AED',
     'MIN_AMOUNT': 100,
     'MAX_AMOUNT': 50000,
-    'SUCCESS_URL': f'{settings.SITE_URL}/payment/tabby/success/',
-    'FAILURE_URL': f'{settings.SITE_URL}/payment/tabby/failure/',
-    'CANCEL_URL': f'{settings.SITE_URL}/payment/tabby/cancel/',
+    # 'SUCCESS_URL': f'{settings.SITE_URL}/payment/tabby/success/',
+    # 'FAILURE_URL': f'{settings.SITE_URL}/payment/tabby/failure/',
+    # 'CANCEL_URL': f'{settings.SITE_URL}/payment/tabby/cancel/',
+    'SUCCESS_URL': f'{settings.SITE_URL}/authorised//',
+    'FAILURE_URL': f'{settings.SITE_URL}/declined/',
+    'CANCEL_URL': f'{settings.SITE_URL}/cancelled/',
 }
