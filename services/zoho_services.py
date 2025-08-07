1d2bf8a0118c2dab294d547b580b391ab399d777
hamed <hamed.alizadegan@gmail.com>
2025-08-07 11:42:00 +0330

    fixed: zoho invoice tax

--- services/zoho_services.py
+++ services/zoho_services.py
@@ -113,7 +113,8 @@ def zoho_invoice_quantity_update(first_name, last_name, email, address, city, li
         payload = {'customer_id': customer_id,
                    "status": "paid",
                    "notes": "Looking forward for your business.",
-                   'line_items': line_items,
+                   "is_inclusive_tax": True,
+                   "line_items": line_items,
                    }
 
         response_item = requests.post(url=url_invoice, headers=headers, json=payload)
