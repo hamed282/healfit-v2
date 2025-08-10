3a5463ee5c01dd133f6048e8ff67ad900e39b545
hamed <hamed.alizadegan@gmail.com>
2025-08-10 14:08:17 +0330

    fixed: zoho invoice tax

--- services/zoho_services.py
+++ services/zoho_services.py
@@ -19,7 +19,7 @@ def zoho_refresh_token(scope):
     return response_refresh
 
 
-def zoho_invoice_quantity_update(first_name, last_name, email, address, city, line_items, shipping_amount,
+def zoho_invoice_quantity_update(first_name, last_name, email, address, city, line_items,
                                  country='United Arab Emirates', customer_id=None):
 
     organization_id = settings.ORGANIZATION_ID
@@ -116,7 +116,7 @@ def zoho_invoice_quantity_update(first_name, last_name, email, address, city, li
                    "is_inclusive_tax": True,
                    # "discount": 30,
                    # "discount_type": "entity_level",
-                   # "shipping_charge": shipping_amount,
+                   "shipping_charge": 50,
                    "line_items": line_items,
                    }
 
